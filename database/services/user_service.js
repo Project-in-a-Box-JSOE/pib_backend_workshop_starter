50d7389375986ecadbd4d2bb46e6aee207ab711e
pib-ucsd <pib@eng.ucsd.edu>
2021-02-06 04:17:29 -0800

    starter code

--- database/services/user_service.js
+++ database/services/user_service.js
@@ -1,5 +1,7 @@
 var env = require('../config/env');
 
-function user_service(db, indexRouter) {}
+function user_service(db, indexRouter) {
+    // ADD WORKSHOP FUNCTIONS HERE
+}
 
 module.exports = user_service;
