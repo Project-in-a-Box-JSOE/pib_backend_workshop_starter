a8b3e4a133d36af43b457ffcafa8640d56f9a974
pib-ucsd <pib@eng.ucsd.edu>
2021-02-06 03:52:05 -0800

    starter code

--- database/config/env.js
+++ database/config/env.js
@@ -1,7 +1,7 @@
 // Database environment setup and connections
 const env = {
     DATABASE_DIALECT: 'sqlite',
-    DATABASE_STORAGE: './doggo.sqlite',
+    DATABASE_STORAGE: './users.sqlite',
     WEB_URL: 'http://localhost:3000/',
 };
 
